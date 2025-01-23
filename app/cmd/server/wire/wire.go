f323860b1b0e2c698799a407d536ef693c35d044
jiaking <2319413860@qq.com>
2025-01-24 01:08:33 +0800

    通过Nunu生成开发脚手架

---
+++ app/cmd/server/wire/wire.go
@@ -0,0 +1,71 @@
+//go:build wireinject
+// +build wireinject
+
+package wire
+
+import (
+	"app/internal/handler"
+	"app/internal/job"
+	"app/internal/repository"
+	"app/internal/server"
+	"app/internal/service"
+	"app/pkg/app"
+	"app/pkg/jwt"
+	"app/pkg/log"
+	"app/pkg/server/http"
+	"app/pkg/sid"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var repositorySet = wire.NewSet(
+	repository.NewDB,
+	//repository.NewRedis,
+	repository.NewRepository,
+	repository.NewTransaction,
+	repository.NewUserRepository,
+)
+
+var serviceSet = wire.NewSet(
+	service.NewService,
+	service.NewUserService,
+)
+
+var handlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
+
+var jobSet = wire.NewSet(
+	job.NewJob,
+	job.NewUserJob,
+)
+var serverSet = wire.NewSet(
+	server.NewHTTPServer,
+	server.NewJobServer,
+)
+
+// build App
+func newApp(
+	httpServer *http.Server,
+	jobServer *server.JobServer,
+	// task *server.Task,
+) *app.App {
+	return app.NewApp(
+		app.WithServer(httpServer, jobServer),
+		app.WithName("demo-server"),
+	)
+}
+
+func NewWire(*viper.Viper, *log.Logger) (*app.App, func(), error) {
+	panic(wire.Build(
+		repositorySet,
+		serviceSet,
+		handlerSet,
+		jobSet,
+		serverSet,
+		sid.NewSid,
+		jwt.NewJwt,
+		newApp,
+	))
+}
